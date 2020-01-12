6a235380cb179e079999998d588be5f41344ebd6
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-01-12 23:45:39 +0330

    WaterSupply Implemented using Generic type

---
+++ src/aquarium/generics/Aquarium.kt
@@ -0,0 +1,33 @@
+package aquarium.generics
+
+fun main(args: Array<String>) {
+    genericExample()
+}
+
+open class WaterSupply(var needsProcessed: Boolean)
+
+class TapWater : WaterSupply(true) {
+    fun addChemicalCleaners() = apply { needsProcessed = false }
+}
+
+class FishStoreWater : WaterSupply(false)
+
+class LakeWater : WaterSupply(true) {
+    fun filter() = apply { needsProcessed = false }
+}
+
+class Aquarium<T : WaterSupply>(val waterSupply: T) {
+    fun addWater() {
+        check(!waterSupply.needsProcessed) { "water supply needs processed" }
+        println("adding water from $waterSupply")
+    }
+}
+
+fun genericExample() {
+    val aquarium = Aquarium(TapWater())
+    aquarium.waterSupply.addChemicalCleaners()
+
+    val aquarium4 = Aquarium(LakeWater())
+    aquarium4.waterSupply.filter()
+    aquarium4.addWater()
+}
